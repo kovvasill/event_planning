222c26423ed295094eb05d2a9ab01ae8bd519488
kovvasill <vasylkovalisko@gmail.com>
2016-05-21 15:49:19 +0300

    1. Creted "Update" method  for events. 2. Added new column "How many hours left till the start of event" on the Home screen. 3. Added "Moment" library to work with timer.

--- event_planning/src/app/components/event/eventCtrl.js
+++ event_planning/src/app/components/event/eventCtrl.js
@@ -7,23 +7,42 @@
             function ($scope, $routeParams, EventsService, UtilityService) {
                 $scope.errorText = '';
                 $scope.evtName = '';
-                $scope.evtDate = '';
+                $scope.evtDate = new Date();
+                $scope.evtDate.setHours($scope.evtDate.getHours() + 2);
+                $scope.evtDate.setSeconds(0);
+                $scope.evtDate.setMilliseconds(0);
                 $scope.evtDescription = '';
                 $scope.evtPrice = 0;
+                $scope.btnText = 'Save';
 
                 if ($routeParams.keyID > 0) {
                     var evt = EventsService.GetEventByKeyID($routeParams.keyID);
                     if (evt) {
                         $scope.evtName = evt.name;
-                        $scope.evtDate = evt.date;
+                        $scope.evtDate = new Date(evt.date);
+                        $scope.evtDate.setSeconds(0);
+                        $scope.evtDate.setMilliseconds(0);
                         $scope.evtDescription = evt.description;
                         $scope.evtPrice = evt.price;
+                        $scope.btnText = 'Update';
                     }
                 }
 
                 $scope.Save = function () {
                     if (UtilityService.StringIsEmpty($scope.evtName)) {
                         $scope.errorText = 'Name is required';
+                    }
+                    else if (new Date($scope.evtDate) <= new Date) {
+                        $scope.errorText = 'Please set date in future';
+                    }
+                    else if ($routeParams.keyID > 0) {
+                        var evt = EventsService.GetEventByKeyID($routeParams.keyID);
+                        evt.name = $scope.evtName;
+                        evt.date = $scope.evtDate;
+                        evt.description = $scope.evtDescription;
+                        evt.price = $scope.evtPrice;
+                        EventsService.SaveEvents();
+                        $scope.LocateTo('/home');
                     }
                     else {
                         EventsService.AddEvent({
---
+++ event_planning/src/app/shared/directives/timeToLeft/timeToLeftDirective.js
@@ -0,0 +1,51 @@
+﻿(function () {
+    'use strict';
+
+    angular.module('evtOrgCtrls').
+        directive('timeToLeft',
+            ['$interval', 'dateFilter',
+            function ($interval, dateFilter) {
+                return {
+                    restrict: 'A',
+                    templateUrl: 'src/app/shared/directives/timeToLeft/timeToLeftView.html',
+                    link: function (scope, element, attrs) {
+                        var evtSchDateTime;
+                        var stopTime; // so that we can cancel the time updates
+                        scope.ttLeft = '';
+                        scope.soonEvt = false; // this value using to assign ng-class (red indication): event will be very soon
+
+                        // used to update the UI
+                        function updateTime() {
+                            if (evtSchDateTime) {
+                                var dtNow = dateFilter(new Date(), 'd/M/yyyy h:mm:ss');
+                                var dtSch = dateFilter(new Date(evtSchDateTime), 'd/M/yyyy h:mm:ss');
+                                var ms = moment(dtSch, "DD/MM/YYYY HH:mm:ss").diff(moment(dtNow, "DD/MM/YYYY HH:mm:ss"));
+                                var s = '';
+                                if (ms > 0) {
+                                    var d = moment.duration(ms);
+                                    s = Math.floor(d.asHours()) + moment.utc(ms).format(":mm:ss");
+                                    if (d.asHours() <= 4) {
+                                        scope.soonEvt = true;
+                                    }
+                                }
+                                scope.ttLeft = s;
+                            }
+                        }
+
+                        // watch the expression, and update the UI on change.
+                        scope.$watch(attrs.timeToLeft, function (value) {
+                            evtSchDateTime = value;
+                            updateTime();
+                        });
+
+                        stopTime = $interval(updateTime, 3000);
+
+                        // listen on DOM destroy (removal) event, and cancel the next UI update
+                        // to prevent updating time after the DOM element was removed.
+                        element.on('$destroy', function () {
+                            $interval.cancel(stopTime);
+                        });
+                    }
+                };
+            }]);
+})();
