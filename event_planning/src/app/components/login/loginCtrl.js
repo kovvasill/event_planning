d2204273b704df6c6a286bc26b6e635ec7a476b4
kovvasill <vasylkovalisko@gmail.com>
2016-05-21 13:20:09 +0300

    Added login control

---
+++ event_planning/src/app/components/login/loginCtrl.js
@@ -0,0 +1,27 @@
+﻿(function () {
+    'use strict';
+
+    angular.module('evtOrgCtrls').
+        controller('LoginCtrl',
+            ['$scope', 'UtilityService', 'UsersService',
+            function ($scope, UtilityService, UsersService) {
+                $scope.errorText = '';
+                $scope.userID = '';
+                $scope.password = '';
+
+                $scope.LogInUser = function () {
+                    if (UtilityService.StringIsEmpty($scope.userID)) {
+                        $scope.errorText = 'User ID is required';
+                    }
+                    else {
+                        if (UsersService.LogInUser($scope.userID, $scope.password)) {
+                            $scope.LocateTo('/home');
+                        }
+                        else {
+                            $scope.errorText = 'User ID or password is incorrect';
+                        }
+                    }
+                }
+            }]);
+
+})();
